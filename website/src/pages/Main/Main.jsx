648a20955a6316da43a188bf7fe9fcef6ce72722
RFN <maytvey@gmail.com>
2026-04-18 19:42:15 +0300

    frontend updated

--- website/src/pages/Main/Main.jsx
+++ website/src/pages/Main/Main.jsx
@@ -4,10 +4,18 @@ export default function App() {
     return (
         <>
 
+        <link rel="icon" href="/icons/wave.svg" />
+
         <div className='headerbox'>
-            
+            <img src="/icons/wave.svg" alt="Logo" />
         </div>
-        
+
+        <div className="content">
+            <div className="content-head">
+                Wave
+            </div>
+        </div>
+
         </>
     );
 }
