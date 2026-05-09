fc2bbea10422cdb39f03ecafc2eae488bcea34e7
RFN <maytvey@gmail.com>
2026-05-09 22:33:26 +0300

    added setting for change avatar

--- backend/api/profile/settings/__init__.py
+++ backend/api/profile/settings/__init__.py
@@ -1,11 +1,26 @@
 from fastapi import APIRouter, Depends, HTTPException, status, Request
 from pydantic import BaseModel
 from backend.dependency import database
+from urllib.parse import urlparse
 router = APIRouter()
 
 class changeAvatar(BaseModel):
     argument: str
 
 @router.post("/profile/changeAvatar/URL")
-async def change_avatar(data: changeAvatar):
-    return {"ok": True}
+async def change_avatar(data: changeAvatar, request: Request, mysql = Depends(database.get_mysql), redis = Depends(database.get_redis)):
+    session_id = request.cookies.get("session_id")
+    print(session_id)
+    value = await redis.get(f"session:{session_id}")
+    
+    if not value:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Not Auth")
+    parsed = urlparse(data.argument)
+    if parsed.scheme not in ("http", "https"):
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid URL")
+    
+    async with mysql.acquire() as conn:
+        async with conn.cursor() as cursor:
+            await cursor.execute("UPDATE users SET avatar_url = %s WHERE id = %s", (data.argument, value,))
+            
+    return {"message": "Avatar updated successfully"}
--- frontend/src/pages/Profile/Settings/utils/changeAvatar.js
+++ frontend/src/pages/Profile/Settings/utils/changeAvatar.js
@@ -2,7 +2,22 @@ export async function changeAvatar(isURL, argument)
 {
     if (isURL)
     {
-        const res = await fetch(`${import.meta.env.VITE_API_URL}/`)
+        const res = await fetch(`${import.meta.env.VITE_API_URL}/profile/changeAvatar/URL`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            credentials: "include",
+            body: JSON.stringify({
+                argument: argument
+            })
+        })
+        if (res.ok)
+        {
+            return true
+        }
+        else {
+            return false
+        }
     }
-    return
 }
