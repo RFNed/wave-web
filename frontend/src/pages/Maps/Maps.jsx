2040709b6215f3e3dd508c329008d493530ef660
RFNed <166419840+RFNed@users.noreply.github.com>
2026-05-11 13:15:24 +0000

    added maps, edited structure for page "Maps"

--- frontend/src/pages/Maps/Maps.jsx
+++ frontend/src/pages/Maps/Maps.jsx
@@ -1,9 +1,18 @@
 
 import { Link } from "react-router-dom"
+import svgSearch from "/assets/icons/search.svg?react"
+import "./Maps.css"
 export default function Maps() {
     return (
         <>
         
+        <div className="map-content">
+
+            <div className="map-search-container">
+                <input className="map-search-input"/>
+                <button className="map-search-button"><div className="fas fa-search" id="search" /></button>
+            </div>
+        </div>
         
         </>
     )
